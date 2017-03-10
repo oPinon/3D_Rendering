343e97e80e4019e3acb05818d898fd6cbe52cfab
Olivier Pinon <oPinon@users.noreply.github.com>
2017-03-09 21:29:14 -0500

    Refactored Mesh to use Vec3 instead of floats

--- src/Mesh.h
+++ src/Mesh.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <array>
+#include <initializer_list>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -14,33 +15,24 @@ template <uint S, typename T>
 struct Vec
 {
 protected:
-	inline const uint size() { return S; }
+	inline const uint size() const { return S; }
 public:
 	const T& operator[]( unsigned int i ) const { return values[i]; }
 	T& operator[]( unsigned int i ) { return values[i]; }
 	void operator+=( const T& v ) { for( uint i = 0; i < size(); i++ ) { values[i] += v; } }
+	Vec operator-( const Vec& v ) const { Vec dst; for( uint i = 0; i < size(); i++ ) { dst[i] = values[i] - v[i]; } return dst; }
+	T norm2() const { T sum = 0; for( uint i = 0; i < size(); i++ ) { sum += values[i] * values[i]; } return sum; }
+	T norm() const { return sqrt( norm2() ); }
+	void operator/=( const T& v ) { for( uint i = 0; i < size(); i++ ) { values[i] /= v; } }
+	Vec operator/( const T& v ) const { Vec dst = ( *this ); dst /= v; return dst; }
+	Vec normalized() const { const T n = norm(); return n == 0 ? Vec() : ( *this ) / n; }
 	T values[S];
 	Vec() { for( uint i = 0; i < size(); i++ ) { values[i] = 0; } }
+	Vec( std::initializer_list<T> l ) { for( uint i = 0; i < size(); i++ ) { values[i] = l.begin()[i]; } } // HACK ?
 };
 
-template <typename T>
-struct _Vec3 : public Vec<3, T>
-{
-	_Vec3() : Vec<3,T>() {}
-	_Vec3( const T& x, const T& y, const T& z ) : Vec<3,T>()
-	{ values[0] = x; values[1] = y; values[2] = z; }
-};
-
-template <typename T>
-struct _Vec4 : public Vec<4, T>
-{
-	_Vec4() : Vec<4,T>() {}
-	_Vec4( const T& x, const T& y, const T& z, const T& w ) : Vec<4, T>()
-	{ values[0] = x; values[1] = y; values[2] = z; values[3] = w; }
-};
-
-typedef _Vec3<float> Vec3F;
-typedef _Vec4<uint> Vec4U;
+typedef Vec<3,float> Vec3F;
+typedef Vec<4,uint> Vec4U;
 
 class Mesh {
 
@@ -57,38 +49,39 @@ protected:
 		Face( const Vec4U& v )
 			: isQuad( true ), v( v )
 		{}
+		inline uint size() const { return isQuad ? 4 : 3; }
 	};
 	struct Line {
 		unsigned int start, end;
 	};
 
-	std::vector<float> vertices, normals;
+	std::vector<Vec3F> vertices, normals;
 	std::vector<Line> lines;
 	std::vector<Face> faces;
 
 public:
 
+	inline uint ptCount() const { return vertices.size(); }
+	inline uint normalCount() const { return normals.size(); }
+
 	inline void addVertex( const Vec3F& v )
 	{
-		for( uint i = 0; i < 3; i++ )
-		{
-			vertices.push_back( v[i] );
-			normals.push_back( 0 );
-		}
+		vertices.push_back( v );
+		normals.push_back( Vec3F() );
 	}
 
 	void scale( const Vec3F& s )
 	{
 		for( uint i = 0; i < vertices.size(); i += 3 )
 			for( uint j = 0; j < 3; j++ )
-				vertices[i + j] *= s[j];
+				vertices[i][j] *= s[j];
 	}
 
 	void translate( const Vec3F& t )
 	{
 		for( uint i = 0; i < vertices.size(); i += 3 )
 			for( uint j = 0; j < 3; j++ )
-				vertices[i + j] += t[j];
+				vertices[i][j] += t[j];
 	}
 
 	void operator+=( const Mesh& m )
@@ -96,15 +89,15 @@ public:
 		for( const auto& l : m.lines )
 		{
 			Line newL = l;
-			newL.start += this->vertices.size()/3;
-			newL.end += this->vertices.size()/3;
+			newL.start += this->ptCount();
+			newL.end += this->ptCount();
 			this->lines.push_back( newL );
 		}
 		for( const auto& f : m.faces )
 		{
 			Face newF = f;
-			newF.v += uint( this->vertices.size()/3 );
-			newF.vn += uint( this->normals.size()/3 );
+			newF.v += this->ptCount();
+			newF.vn += this->ptCount();
 			//newF.vt += // TODO
 			this->faces.push_back( newF );
 		}
@@ -162,9 +155,7 @@ public:
 							std::stringstream ss(line.substr(1));
 							float x, y, z;
 							ss >> x >> y >> z;
-							mesh.vertices.push_back(x);
-							mesh.vertices.push_back(y);
-							mesh.vertices.push_back(z);
+							mesh.vertices.push_back( { x, y, z } );
 							break;
 						}
 
@@ -173,9 +164,7 @@ public:
 							std::stringstream ss(line.substr(2));
 							float x, y, z;
 							ss >> x >> y >> z;
-							mesh.normals.push_back(x);
-							mesh.normals.push_back(y);
-							mesh.normals.push_back(z);
+							mesh.normals.push_back( { x, y, z } );
 							break;
 						}
 					}
@@ -206,23 +195,19 @@ public:
 
 	void init() {
 
+		const std::vector<uint>
+			quadIndices = { 0, 1, 2, 0, 2, 3 },
+			triaIndices = { 0, 1, 2 };
+
 		// Sending Faces' vertices
 		std::vector<float> faceVerts, faceNorms;
 		for(const Face& face : faces) {
-			copyV3(this->vertices.data()+3*face.v[0], faceVerts);
-			copyV3(this->vertices.data()+3*face.v[1], faceVerts);
-			copyV3(this->vertices.data()+3*face.v[2], faceVerts);
-			copyV3(this->normals.data()+3*face.vn[0], faceNorms);
-			copyV3(this->normals.data()+3*face.vn[1], faceNorms);
-			copyV3(this->normals.data()+3*face.vn[2], faceNorms);
-			if(face.isQuad) {
-				copyV3(this->vertices.data()+3*face.v[0], faceVerts);
-				copyV3(this->vertices.data()+3*face.v[2], faceVerts);
-				copyV3(this->vertices.data()+3*face.v[3], faceVerts);
-				copyV3(this->normals.data()+3*face.vn[0], faceNorms);
-				copyV3(this->normals.data()+3*face.vn[2], faceNorms);
-				copyV3(this->normals.data()+3*face.vn[3], faceNorms);				
-			}
+			for( uint v : ( face.isQuad ? quadIndices : triaIndices ) )
+				for( uint i = 0; i < 3; i++ )
+				{
+					faceVerts.push_back( this->vertices[face.v[v]][i] );
+					faceNorms.push_back( this->normals[face.vn[v]][i] );
+				}
 		}
 		nbVertTris = faceVerts.size()/3;
 
@@ -237,19 +222,19 @@ public:
 		// Sending Lines' vertices
 		std::vector<float> lineVerts, lineNorms;
 		for(const Line& line : this->lines) {
-			float* start = this->vertices.data() + 3*line.start;
-			float* end = this->vertices.data() + 3*line.end;
-			float dx = start[0]-end[0], dy = start[1]-end[1], dz = start[2]-end[2];
-			float norm = sqrt(dx*dx+dy*dy+dz*dz);
-			if(norm > 0) {
-				dx /= norm;
-				dy /= norm;
-				dz /= norm;
+			const Vec3F startV = this->vertices[line.start];
+			const Vec3F endV = this->vertices[line.end];
+			const Vec3F normal = ( endV - startV ).normalized();
+			for( uint i = 0; i < 3; i++ )
+			{
+				lineVerts.push_back( startV[i] );
+				lineNorms.push_back( normal[i] );
 			}
-			copyV3(start,lineVerts);
-			copyV3(end,lineVerts);
-			lineNorms.push_back(dx); lineNorms.push_back(dy); lineNorms.push_back(dz);
-			lineNorms.push_back(dx); lineNorms.push_back(dy); lineNorms.push_back(dz);
+			for( uint i = 0; i < 3; i++ )
+			{
+				lineVerts.push_back( endV[i] );
+				lineNorms.push_back( normal[i] );
+			}
 		}
 		nbVertLines = lineVerts.size()/3;
 
